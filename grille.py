837d23a850b0c99e5c87024d0f8bbf1181d08644
Emanuel <emanuelmrl13@gmail.com>
2022-05-17 11:17:37 +0200

    [update] nouvelle methodes et clean

--- grille.py
+++ grille.py
@@ -9,7 +9,13 @@ class Grille:
         """
         self.grille = [[0 for i in range(10)] for j in range(40)]
 
-    def supprime_ligne(self, ligne: int) -> None:
+    def nouvelle_grille(self) -> None:
+        """
+        initialise la grille avec des 0
+        """
+        self.grille = [[0 for i in range(10)] for j in range(40)]
+
+    def supprimer_ligne(self, ligne: int) -> None:
         """
         supprime une ligne
         """
@@ -17,10 +23,34 @@ class Grille:
             self.grille[i] = self.grille[i - 1]
         self.grille[0] = [0 for i in range(10)]
 
-    def verifie_ligne(self) -> None:
+    def verifier_lignes(self) -> None:
         """
         verifie si une ligne est completee et la supprime si c'est le cas
         """
         for i in range(40):
             if 0 not in self.grille[i]:
-                self.supprime_ligne(i)
+                self.supprimer_ligne(i)
+
+    def peut_placer(self, piece) -> bool:
+        """
+        verifie si une piece peut etre placee sur la grille
+        """
+        for i in range(piece.taille):
+            for j in range(piece.taille):
+                if piece.piece[i][j] != 0 and (
+                    piece.x + j < 0
+                    or piece.x + j > 9
+                    or piece.y + i > 39
+                    or self.grille[piece.y + i][piece.x + j] != 0
+                ):
+                    return False
+        return True
+
+    def placer(self, piece) -> None:
+        """
+        place une piece sur la grille
+        """
+        for i in range(piece.taille):
+            for j in range(piece.taille):
+                if piece.piece[i][j] != 0:
+                    self.grille[piece.y + i][piece.x + j] = piece.piece[i][j]
